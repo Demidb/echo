66aba36aae784600ab10547033c8651bf3007928
Demidb <144897128+Demidb@users.noreply.github.com>
2024-07-09 11:33:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ dp = Dispatcher(bot)
  
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-   await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+   await message.reply("кукусики")
  
 @dp.message_handler()
 async def echo(message: types.Message):
