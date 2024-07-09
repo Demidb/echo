1bf157223d3bc88a780ce519dc7d1a85ba64fced
Demidb <144897128+Demidb@users.noreply.github.com>
2024-07-09 11:33:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+API_TOKEN = '7462583146:AAEFAkFUeMlVGmWPBMLeVmpHR5ZCE6LZRG4'
+ 
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+ 
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+   await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+ 
+@dp.message_handler()
+async def echo(message: types.Message):
+   await message.answer(message.text)
+ 
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
